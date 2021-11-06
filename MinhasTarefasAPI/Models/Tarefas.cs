16121c2222d2fc89622f7e6be95eb6420425faa4
Thiago Goulart <tyagosbryto3321@gmail.com>
2021-11-05 21:03:47 -0300

    Initial Commit

---
+++ MinhasTarefasAPI/Models/Tarefas.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MinhasTarefasAPI.Models
+{
+    public class Tarefas
+    {
+        public int Id { get; set; }
+        public string Titulo { get; set; }
+        public string Local { get; set; }
+        public string Descricao { get; set; }
+        public string Tipo { get; set; }
+        public bool Concluido { get; set; }
+        public DateTime DataHora { get; set; }
+        public DateTime Criado { get; set; }
+        public DateTime Atualizado { get; set; }
+
+    }
+}
